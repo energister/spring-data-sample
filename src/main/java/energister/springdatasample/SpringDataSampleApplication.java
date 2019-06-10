581084c23d459770744783319180cf360253b5fd
energister <energister@users.noreply.github.com>
2019-06-11 01:40:50 +0300

    Spring Boot sample app

---
+++ src/main/java/energister/springdatasample/SpringDataSampleApplication.java
@@ -0,0 +1,13 @@
+package energister.springdatasample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataSampleApplication.class, args);
+	}
+
+}
